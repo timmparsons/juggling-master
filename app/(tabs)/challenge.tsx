3bad252017e82ffd8df2654ad1e0e118ca0cadfe
timmparsons <tim.m.parsons@hotmail.com>
2025-05-13 22:45:13 -0700

    added challenges

--- app/(tabs)/challenge.tsx
+++ app/(tabs)/challenge.tsx
@@ -1,21 +1,58 @@
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import React from 'react';
 import { PADDING, TYPOGRAPHY } from '../../theme';
+import { challenges } from '@/components/fixtures/challenges';
+
+type ItemProps = {
+  title: string;
+  description: string;
+};
+
+const ChallengeCard = ({ title, description }: ItemProps) => {
+  return (
+    <View style={styles.challengeContainer}>
+      <Text style={styles.challengeTitle}>{title}</Text>
+      <Text style={styles.challengeDescription}>{description}</Text>
+      <Button
+        title='Join Challenge'
+        onPress={() => console.log('Catch on foot')}
+      />
+    </View>
+  );
+};
 
 const Challenge = () => {
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <Text style={styles.header}>Challenge</Text>
-        <View style={styles.challengeContainer}>
-          <Text style={styles.challengeTitle}>Catch on Foot</Text>
-          <Text style={styles.challengeDescription}>
-            One of the simplest juggling tricks to perform. You got this!
-          </Text>
-          <Button
-            title='Join Challenge'
-            onPress={() => console.log('Catch on foot')}
+        <Text style={styles.header}>Challenges</Text>
+        <View style={styles.rowContainer}>
+          <FlatList
+            data={challenges}
+            horizontal
+            renderItem={({ item }) => (
+              <ChallengeCard
+                title={item.title}
+                description={item.description}
+              />
+            )}
           />
+        </View>
+        <View>
+          <Text>Event</Text>
+          <Text>Juggle to 5</Text>
+          <Text>Juggle to 10</Text>
+          <Text>Juggle to 20</Text>
+          <Text>Juggle to 35</Text>
+          <Text>Juggle to 50</Text>
+          <Text>Juggle to 100</Text>
         </View>
       </View>
     </SafeAreaView>
@@ -30,21 +67,27 @@ const styles = StyleSheet.create({
   },
   header: {
     fontSize: 40,
-    fontWeight: 300,
-    // padding: SPACING.paddingLeft,
+    fontWeight: '300',
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
   },
   challengeContainer: {
-    marginTop: 20,
-    width: '70%',
+    // width: '48%', // Two containers side by side
     backgroundColor: 'lightblue',
     padding: 15,
     borderRadius: 9,
+    marginHorizontal: 4,
   },
   challengeTitle: {
     fontSize: 20,
     fontWeight: 'bold',
+    flexWrap: 'wrap',
   },
   challengeDescription: {
     paddingTop: 15,
+    flexWrap: 'wrap',
   },
 });
---
+++ components/fixtures/challenges.tsx
@@ -0,0 +1,10 @@
+export const challenges = [
+  {
+    title: 'Catch on Foot',
+    description: 'One of the simplest tricks to perform.',
+  },
+  {
+    title: 'Catch on back of neck',
+    description: "This one's a little tougher.",
+  },
+];
